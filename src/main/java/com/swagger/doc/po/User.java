d67ccb305354c4d8d08e8c69e04560daa75777e6
wansboods <wansboods@gmail.com>
2018-05-11 16:18:09 +0800

    swagger

--- src/main/java/com/swagger/doc/po/User.java
+++ src/main/java/com/swagger/doc/po/User.java
@@ -1,11 +1,16 @@
 package com.swagger.doc.po;
 
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * Created by haoyifen on 16-9-28 2016 下午5:53
  */
 public class User {
+    @ApiModelProperty(value="用户ID" ,notes="表用户ID")
     private int userId;
+    @ApiModelProperty(value="用户名称" ,notes="表用户名称")
     private String name;
+    @ApiModelProperty(value="用户年龄" ,notes="表用户年龄")
     private int age;
 
     public User() {
